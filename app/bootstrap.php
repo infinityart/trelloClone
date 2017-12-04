25d816554852c612fb3ab1bfbfc1f6e589862725
infinityart <jonty_wolf@hotmail.com>
2017-12-04 20:51:55 +0100

    Added http component

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -23,3 +23,15 @@ if ($environment !== 'production') {
     });
 }
 $whoops->register();
+
+$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new \Http\HttpResponse;
+
+$content = '<h1>Hello World</h1>';
+$response->setContent($content);
+
+foreach ($response->getHeaders() as $header) {
+    header($header, false);
+}
+
+echo $response->getContent();
