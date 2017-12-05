70985fb91e315f8963fadc38890debb98b3142f6
infinityart <jonty_wolf@hotmail.com>
2017-12-05 23:04:57 +0100

    Added dependency injector

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -24,9 +24,10 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
+$injector = include('Dependencies.php');
 
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new \Http\HttpResponse;
+$request = $injector->make('Http\HttpRequest');
+$response = $injector->make('Http\HttpResponse');
 
 /**
  * Router start..
@@ -61,7 +62,7 @@ switch ($routeInfo[0]) {
         $method = $routeInfo[1][1];
         $vars = $routeInfo[2];
 
-        $class = new $className($response);
+        $class = $injector->make($className);
         $class->$method($vars);
         break;
 }
---
+++ app/dependencies.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: Jonty
+ * Date: 12/5/2017
+ */
+declare(strict_types = 1);
+
+$injector = new \Auryn\Injector;
+
+$injector->alias('Http\Request', 'Http\HttpRequest');
+$injector->share('Http\HttpRequest');
+$injector->define('Http\HttpRequest', [
+    ':get' => $_GET,
+    ':post' => $_POST,
+    ':cookies' => $_COOKIE,
+    ':files' => $_FILES,
+    ':server' => $_SERVER,
+]);
+
+$injector->alias('Http\Response', 'Http\HttpResponse');
+$injector->share('Http\HttpResponse');
+
+return $injector;
